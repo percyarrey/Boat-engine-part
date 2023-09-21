23d8c44de851df1ddb4b308177268acc1f07b398
percyarrey <tanyitikuarrey@gmail.com>
2023-09-21 15:06:01 +0100

    new

--- services/getProducts.js
+++ services/getProducts.js
@@ -4,7 +4,7 @@ import connectDB from "../utils/connectDB";
 export const getProducts = async(num=0)=>{
     try{
         await connectDB()
-        var data = await Products.find({}).limit(11).skip(num);
+        var data = await Products.find({}).limit(5).skip(num);
         data = data.map((e,index)=>{
             e.image={img1:e.image.img1}
             return e
