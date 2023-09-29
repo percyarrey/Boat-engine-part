e5fe44bde358bd4b3c0d821ae8a18bfc3c94656e
percyarrey <tanyitikuarrey@gmail.com>
2023-09-29 07:00:39 +0100

    env

--- app/trackorder/[id]/page.js
+++ app/trackorder/[id]/page.js
@@ -4,9 +4,13 @@ import React from 'react';
 
 import TrackOrder from '../../components/trackorder/trackorder'
 import { getOrder } from '../../../services/getOrder';
+import { redirect } from 'next/navigation'
 
 export default async function Page({params}) {
   var data = await getOrder(params.id)
+  if(!data){
+    redirect('/deals')
+  }
   return (
     <>
       <TrackOrder data={data} />
