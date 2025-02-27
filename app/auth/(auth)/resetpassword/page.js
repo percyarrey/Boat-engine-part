4f1846a7fb553a2e7a15e90af97ea4f7bf04d803
percyarrey <tanyitikuarrey@gmail.com>
2025-02-26 16:43:21 -0800

    fix reset pass

--- app/auth/(auth)/resetpassword/page.js
+++ app/auth/(auth)/resetpassword/page.js
@@ -1,44 +1,57 @@
-'use client';
+"use client";
 
-import React, { useState,useCallback,useEffect } from 'react';
-import { toast } from 'react-toastify';
-import { Button } from '@mui/material';
-import Link from 'next/link';
-import { resetPassword, verifyResetPasswordCode } from '../../../../server actions/auth/actions';
-import{ useSearchParams,useRouter,usePathname} from 'next/navigation';
+import React, { useState, useCallback, useEffect } from "react";
+import { toast } from "react-toastify";
+import { Button } from "@mui/material";
+import Link from "next/link";
+import {
+  resetPassword,
+  verifyResetPasswordCode,
+} from "../../../../server actions/auth/actions";
+import { useSearchParams, useRouter, usePathname } from "next/navigation";
 
-export default function ResetPassword({searchParams}) {
+export default function ResetPassword({ searchParams }) {
   const router = useRouter();
-  const id = searchParams.id
-  const pathname = usePathname()
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+  const id = searchParams.id;
+  const pathname = usePathname();
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [loading, setLoading] = useState(false);
   const [isCodeVerified, setisCodeVerified] = useState(false);
-  const [code, setCode] = useState(searchParams.code ||'');
+  const [code, setCode] = useState(searchParams.code || "");
 
-//CREATE QUERY STRING
-const createQueryString = useCallback(
-  (name, value) => {
-    const params = new URLSearchParams(searchParams)
-    params.set(name, value)
+  //CREATE QUERY STRING
+  const createQueryString = useCallback(
+    (name, value) => {
+      const params = new URLSearchParams(searchParams);
+      params.set(name, value);
 
-    return params.toString()
-  },
-  [searchParams]
-)
+      return params.toString();
+    },
+    [searchParams]
+  );
 
-//VERIFY CODE 
-const verifyCode = async (e) => {
-
-  if (code.length === 4 && id) {
-    try {
-      const result = await verifyResetPasswordCode(id, code);
-      if(result.success){
-        router.push(`${pathname}?${createQueryString('code', code)}`);
-      setisCodeVerified(true);
-      }else{
-        toast.warn(result.message, {
+  //VERIFY CODE
+  const verifyCode = async (e) => {
+    if (code.length === 4 && id) {
+      try {
+        const result = await verifyResetPasswordCode(id, code);
+        if (result.success) {
+          router.push(`${pathname}?${createQueryString("code", code)}`);
+          setisCodeVerified(true);
+        } else {
+          toast.warn(result.message, {
+            position: "top-center",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            theme: "light",
+          });
+        }
+      } catch (error) {
+        toast.warn("Something went wrong. Try again!", {
           position: "top-center",
           autoClose: 2000,
           hideProgressBar: false,
@@ -48,26 +61,14 @@ const verifyCode = async (e) => {
           theme: "light",
         });
       }
-      
-    } catch (error) {
-      toast.warn('Something went wrong. Try again!', {
-        position: "top-center",
-        autoClose: 2000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        theme: "light",
-      });
     }
-  }
-};
+  };
 
-useEffect(() => {
-  if (code.length === 4 && id) {
-    verifyCode();
-  }
-});
+  useEffect(() => {
+    if (code.length === 4 && id) {
+      verifyCode();
+    }
+  });
   // Handle password change
   function handlePasswordChange(e) {
     setPassword(e.target.value);
@@ -92,7 +93,7 @@ useEffect(() => {
       errors.push(`at least ${minLength} characters long`);
     }
     if (!/[A-Z]/.test(password)) {
-      errors.push('at least one uppercase letter');
+      errors.push("at least one uppercase letter");
     }
 
     return errors;
@@ -115,9 +116,9 @@ useEffect(() => {
             try {
               // Simulate API call for resetting the password
               try {
-                const result = await resetPassword(id, code,password);
-                if(result.success){
-                  toast.success('Password reset successfully!', {
+                const result = await resetPassword(id, code, password);
+                if (result.success) {
+                  toast.success("Password reset successfully!", {
                     position: "top-center",
                     autoClose: 2000,
                     hideProgressBar: false,
@@ -126,11 +127,11 @@ useEffect(() => {
                     draggable: true,
                     theme: "light",
                   });
-    
+
                   setTimeout(() => {
-                    router.push('/auth/login'); // Redirect to login page after success
+                    router.push("/auth/login"); // Redirect to login page after success
                   }, 500);
-                }else{
+                } else {
                   toast.warn(result.message, {
                     position: "top-center",
                     autoClose: 2000,
@@ -141,10 +142,9 @@ useEffect(() => {
                     theme: "light",
                   });
                 }
-                
               } catch (error) {
-                console.error(error)
-                toast.warn('Something went wrong. Try again!', {
+                console.error(error);
+                toast.warn("Something went wrong. Try again!", {
                   position: "top-center",
                   autoClose: 2000,
                   hideProgressBar: false,
@@ -154,10 +154,8 @@ useEffect(() => {
                   theme: "light",
                 });
               }
-
-              
             } catch (error) {
-              toast.error('Something went wrong! Please try again.', {
+              toast.error("Something went wrong! Please try again.", {
                 position: "top-center",
                 autoClose: 2000,
                 hideProgressBar: false,
@@ -170,7 +168,7 @@ useEffect(() => {
               setLoading(false);
             }
           } else {
-            toast.warn(`Password must include: ${passwordErrors.join(', ')}.`, {
+            toast.warn(`Password must include: ${passwordErrors.join(", ")}.`, {
               position: "top-center",
               autoClose: 2000,
               hideProgressBar: false,
@@ -181,7 +179,7 @@ useEffect(() => {
             });
           }
         } else {
-          toast.warn('Passwords do not match.', {
+          toast.warn("Passwords do not match.", {
             position: "top-center",
             autoClose: 2000,
             hideProgressBar: false,
@@ -192,7 +190,7 @@ useEffect(() => {
           });
         }
       } else {
-        toast.warn('Please fill in all fields.', {
+        toast.warn("Please fill in all fields.", {
           position: "top-center",
           autoClose: 2000,
           hideProgressBar: false,
@@ -203,19 +201,24 @@ useEffect(() => {
         });
       }
     } else {
-      verifyCode()  
+      verifyCode();
     }
   }
 
   return (
-    <section className='flex min-h-[95vh] justify-center items-center'>
+    <section className="flex min-h-[95vh] justify-center items-center">
       <div className="w-full max-w-md p-6 bg-white rounded-lg shadow-md">
         {isCodeVerified ? (
           <>
             <h1 className="text-2xl font-bold text-center">Reset Password</h1>
             <form onSubmit={handleSubmit} className="space-y-4 mt-5">
               <div>
-                <label htmlFor="password" className="block mb-2 text-md font-medium text-gray-900">New Password</label>
+                <label
+                  htmlFor="password"
+                  className="block mb-2 text-md font-medium text-gray-900"
+                >
+                  New Password
+                </label>
                 <input
                   type="password"
                   name="password"
@@ -228,7 +231,12 @@ useEffect(() => {
                 />
               </div>
               <div>
-                <label htmlFor="confirm-password" className="block mb-2 text-md font-medium text-gray-900">Confirm New Password</label>
+                <label
+                  htmlFor="confirm-password"
+                  className="block mb-2 text-md font-medium text-gray-900"
+                >
+                  Confirm New Password
+                </label>
                 <input
                   type="password"
                   name="confirm-password"
@@ -240,8 +248,14 @@ useEffect(() => {
                   required
                 />
               </div>
-              <Button type="submit" style={{ width: '100%', backgroundColor: '#00442E' }} variant='contained' color='success' fullWidth>
-                {loading ? 'Resetting...' : 'Reset Password'}
+              <Button
+                type="submit"
+                style={{ width: "100%", backgroundColor: "#00442E" }}
+                variant="contained"
+                color="success"
+                fullWidth
+              >
+                {loading ? "Resetting..." : "Reset Password"}
               </Button>
             </form>
           </>
@@ -249,11 +263,17 @@ useEffect(() => {
           <>
             <h1 className="text-2xl font-bold text-center">Enter Code</h1>
             <p className="text-center">
-              Check your email for a code to reset your password. If it doesn’t appear within a few minutes, check your spam folder.
+              Check your email for a code to reset your password. If it doesn’t
+              appear within a few minutes, check your spam folder.
             </p>
             <form onSubmit={handleSubmit} className="space-y-4 mt-5">
               <div>
-                <label htmlFor="code" className="block mb-2 text-md font-medium text-gray-900">Code</label>
+                <label
+                  htmlFor="code"
+                  className="block mb-2 text-md font-medium text-gray-900"
+                >
+                  Code
+                </label>
                 <input
                   type="text"
                   name="code"
@@ -266,24 +286,39 @@ useEffect(() => {
                   required
                 />
               </div>
-              <div className='text-end'>{"Didn't"} receive a code, <Link href={'/auth/forgotpassword'} className='text-blue-500'>Try Again</Link></div>
+              <div className="text-end">
+                {"Didn't"} receive a code,{" "}
+                <Link href={"/auth/forgotpassword"} className="text-blue-500">
+                  Try Again
+                </Link>
+              </div>
 
               <Button
-  type="submit"
-  style={code.length === 4 ? { width: '100%', backgroundColor: '#00442E' } : { width: '100%', backgroundColor: 'gray' }}
-  disabled={code.length !== 4} // Disable the button if the code length is not 4
-  variant='contained'
-  color='success'
-  fullWidth
->
-  {loading ? 'Checking...' : 'Check Code'}
-</Button>
+                type="submit"
+                style={
+                  code.length === 4
+                    ? { width: "100%", backgroundColor: "#00442E" }
+                    : { width: "100%", backgroundColor: "gray" }
+                }
+                disabled={code.length !== 4} // Disable the button if the code length is not 4
+                variant="contained"
+                color="success"
+                fullWidth
+              >
+                {loading ? "Checking..." : "Check Code"}
+              </Button>
             </form>
           </>
         )}
         <div className="mt-4 text-center">
           <p className="text-md font-light text-gray-500">
-            Remembered your password? <Link href="/auth/login" className="font-semibold text-blue-600 hover:underline">Log in</Link>
+            Remembered your password?{" "}
+            <Link
+              href="/auth/login"
+              className="font-semibold text-blue-600 hover:underline"
+            >
+              Log in
+            </Link>
           </p>
         </div>
       </div>
