9b9f3ae9ca19246ba7a5f4d369c4b23879e50a56
percyarrey <110921889+percyarrey@users.noreply.github.com>
2025-03-14 07:58:41 +0100

    Update layout.js

--- app/admin/(admin)/layout.js
+++ app/admin/(admin)/layout.js
@@ -6,7 +6,7 @@ import { redirect } from 'next/navigation'
 export default async function PrivateLayout({ children }) {
     const session = await getServerSession(authOptions)
     if (session?.user) {
-        if (!session?.user?.isVerified) { redirect('/auth/verifyemail') }
+        /*if (!session?.user?.isVerified) { redirect('/auth/verifyemail') }*/
         if (session?.user?.role !== 'admin') { redirect('/') }
     } else {
         redirect('/auth/login')
